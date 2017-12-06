05f627b585ee64392611fcabe22461afda14c26f
John Blazic <johnvblazic@email.arizona.edu>
2017-12-06 02:12:57 -0700

    adding next steps. images are showing, linking not working properly yet

--- data_munging/munge.py
+++ data_munging/munge.py
@@ -1,7 +1,25 @@
 import csv
+import glob
 
-data = {}
 
-cat = "mnist"
-with open("run_100-50-mnist-reg-1,tag_discrimiator_loss.csv", 'rb') as csvfile:
-	data[]
+with open("../loss_data.js","w") as b:
+	b.write("loss_data = [\n")
+	for f_path in glob.glob("../loss_data/*loss.csv"):
+		#get data category and loss type
+		split = f_path.split("_")
+		data_cat = split[1].split("/")[1]
+		with open(f_path,'r') as a:
+			reader = csv.reader(a,delimiter=",")
+			next(reader,None)
+			for row in reader:
+				time = row[0]
+				time_step = row[1]
+				gen_loss = row[2]
+				disc_loss = row[5]
+				image0 = "%s_epoch_%s_cat_0.png" % (str(data_cat), str(int(int(time_step)/200)))
+				image1 = "%s_epoch_%s_cat_1.png" % (str(data_cat), str(int(int(time_step)/200)))
+				image2 = "%s_epoch_%s_cat_2.png" % (str(data_cat), str(int(int(time_step)/200)))
+				write_string = "{ time_step:%s, data_category:\"%s\", gen_loss:%s, disc_loss:%s, image0:\"%s\", image1:\"%s\", image2:\"%s\" },\n" % (str(time_step) ,str(data_cat),str(disc_loss),str(gen_loss),str(image0),str(image1),str(image2))
+				b.write(write_string)
+
+
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var upperChartHeight = 840
+var upperChartWidth = 1300
+var lowerChartHeight = 400 
+var lowerChartWidth = 650
+
 /*
 Found at http://bl.ocks.org/jfreels/6734025
 */
@@ -43,39 +48,52 @@ var colorCat = d3.scaleOrdinal()
 						.range(['#66c2a5','#fc8d62','#8da0cb'])
 
 
-cX = d3.scaleLinear()
+cXPCA = d3.scaleLinear()
 		.domain(d3.extent(pca_data,function(d){return d.pc1;}))
-		.range([30,420]);
-cY = d3.scaleLinear()
+		.range([40,(lowerChartWidth-40)]);
+cYPCA = d3.scaleLinear()
 		.domain(d3.extent(pca_data,function(d){return d.pc2;}))
-		.range([370,30]);
-var bAxis = d3.axisBottom(cX)
-var lAxis = d3.axisLeft(cY)
+		.range([(lowerChartHeight-40),30]);
+cXTIME = d3.scaleLinear()
+    .domain(d3.extent(loss_data,function(d){return d.time_step;}))
+    .range([30,(2*upperChartWidth/3)]);
+cYGEN = d3.scaleLinear()
+    .domain(d3.extent(loss_data,function(d){return d.gen_loss;}))
+    .range([upperChartHeight-30,(upperChartHeight/2)+30]);
+cYDISC = d3.scaleLinear()
+    .domain(d3.extent(loss_data,function(d){return d.disc_loss;}))
+    .range([(upperChartHeight/2)-30,30]);
 
+var bAxis = d3.axisBottom(cXPCA)
+var lAxis = d3.axisLeft(cYPCA)
+var bAxis2 = d3.axisBottom(cXTIME)
+var lAxis2 = d3.axisLeft(cYGEN)
+var lAxis3 = d3.axisLeft(cYDISC)
 
+//LOWER CHART
 var chart4 = d3.select("#scatterplot_1")
 var svg4 = chart4.append("svg")
-/*var brush = svg4.append("g")
+var brush = svg4.append("g")
       .attr("class", "brush")
       .call(d3.brush()
-        .extent([[0, 0], [450, 400]])
-        .on("start brush end", brushed));*/
-svg4.attr("width", 450).attr("height", 400).attr("class","chart2")
+        .extent([[0, 0], [lowerChartWidth, lowerChartHeight]])
+        .on("start brush end", brushed));
+svg4.attr("width", lowerChartWidth).attr("height", lowerChartHeight).attr("class","chart2")
 svg4.append("g")
-	.attr("transform","translate(0,380)")
+	.attr("transform","translate(0," + (lowerChartHeight-40) + ")")
 	.attr("id","x-axis")
 	.call(bAxis)
 svg4.append("g")
 	.attr("id","y-axis")
-	.attr("transform","translate(30,10)")
+	.attr("transform","translate(40,0)")
 	.call(lAxis)
 circle = svg4.selectAll("circle")
 	.data(pca_data)
 	.enter()
 	.append("circle")
     .attr("class", "node")
-	.attr("cx", function(d) { return cX(d.pc1);})
-	.attr("cy", function(d) { return cY(d.pc2); })
+	.attr("cx", function(d) { return cXPCA(d.pc1);})
+	.attr("cy", function(d) { return cYPCA(d.pc2); })
     .attr("r", function(d) { return 4; })
     .attr("stroke", function() { return "black" })
     .attr("stroke-width", function() { return 0.5  })
@@ -84,3 +102,136 @@ circle = svg4.selectAll("circle")
         tabulate([d],['time_step','data_category','lat_category','pc1', 'pc2'])
     });
 
+//UPPER CHARTS
+var chart5 = d3.select("#chart1")
+var svg5 = chart5.append("svg")
+/*var brush2 = svg5.append("g")
+      .attr("class", "brush")
+      .call(d3.brush()
+        .extent([[0, 0], [450, 400]])
+        .on("start brush end", brushed2));*/
+svg5.attr("width", upperChartWidth).attr("height", upperChartHeight).attr("class","chart2")
+svg5.append("g")
+    .attr("class", "circle")
+    .attr("transform","translate(0," + (upperChartHeight-20) + ")")
+    .attr("id","x-axis")
+    .call(bAxis2)
+svg5.append("g")
+    .attr("id","y-axis")
+    .attr("transform","translate(30,0)")
+    .call(lAxis2)
+circle2 = svg5.selectAll("circle")
+    .data(loss_data)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return cXTIME(d.time_step);})
+    .attr("cy", function(d) { return cYGEN(d.gen_loss);})
+    .attr("r", function(d) { return 8; })
+    .attr("stroke", function() { return "black"; })
+    .attr("stroke-width", function() { return 1;  })
+    .style("fill", function(d) { return colorCat(d.data_category)} )
+    .on("click",function(d) {
+      chart5.selectAll("image").remove()
+      showImage(d.image0, 900, 0)
+      showImage(d.image1, 900, 280)
+      showImage(d.image2, 900, 560)
+    });
+
+
+
+svg5.append("g")
+    .attr("class", "circle")
+    .attr("transform","translate(0," + (upperChartHeight/2-20) + ")")
+    .attr("id","x-axis")
+    .call(bAxis2)
+svg5.append("g")
+    .attr("id","y-axis")
+    .attr("transform","translate(30,0)")
+    .call(lAxis3)
+circle3 = svg5.selectAll("circle2")
+    .data(loss_data)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return cXTIME(d.time_step);})
+    .attr("cy", function(d) { return cYDISC(d.disc_loss);})
+    .attr("r", function(d) { return 8; })
+    .attr("stroke", function() { return "black"; })
+    .attr("stroke-width", function() { return 1;  })
+    .style("fill", function(d) { return colorCat(d.data_category)} )
+    .on("click",function(d) {
+      chart5.selectAll("image").remove()
+      showImage(d.image0, 900, 0)
+      showImage(d.image1, 900, 280)
+      showImage(d.image2, 900, 560)
+    });
+
+
+svg4.append("text")
+        .attr("x", (lowerChartWidth / 2) + 70)             
+        .attr("y", 30)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("The First 2 Principal Components");
+
+/*svg5.append("text")
+        .attr("x", (upperChartWidth / 2))             
+        .attr("y", 30)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("Generator Loss vs Training Updates");
+
+svg5.append("text")
+        .attr("x", (upperChartWidth / 2))             
+        .attr("y", (upperChartHeight / 2) + 30)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("Discriminator Loss vs Training Updates");**/
+
+
+
+function brushed() {
+    /*if (brushCell !== this) {
+      d3.select(brushCell).call(brush.move,null);
+      brushCell = this;
+    }*/
+    var selection = d3.event.selection;
+    circle.classed("selected", selection && function(d) {
+      return cXPCA.invert(selection[0][0]) <= d.pc1 && d.pc1 < cXPCA.invert(selection[1][0])
+          && cYPCA.invert(selection[0][1]) >= d.pc2 && d.pc2 > cYPCA.invert(selection[1][1]);    });
+    d3.selectAll("circle")
+      .filter(".selected")
+}
+
+function classed(data) {
+  crircle = d3.selectAll("circle")
+    circle.classed("selected", circle && function(d){
+      return data.time_step == d.time_step && data.data_category == d.data_category;
+    })
+}
+
+function showImage(image, x, y) {
+  console.log(image)
+  svg5.append("svg:image")
+    .attr("xlink:href", "file:images/" + image)
+    .attr("x", x)
+    .attr("y", y)
+    .attr("width", 280)
+    .attr("height", 280);
+}
+
+/*function brushed2() {
+    /*if (brushCell !== this) {
+      d3.select(brushCell).call(brush.move,null);
+      brushCell = this;
+    }
+    var selection = d3.event.selection;
+    circle.classed("selected", selection && function(d) {
+      return cXACT.invert(selection[0][0]) <= d.ACT && d.ACT < cXACT.invert(selection[1][0])
+          && cYGPA.invert(selection[0][1]) >= d.GPA && d.GPA > cYGPA.invert(selection[1][1]);    });
+    circle2.classed("selected", selection && function(d) {
+      return cXACT.invert(selection[0][0]) <= d.ACT && d.ACT < cXACT.invert(selection[1][0])
+          && cYGPA.invert(selection[0][1]) >= d.GPA && d.GPA > cYGPA.invert(selection[1][1]);    });
+}*/
